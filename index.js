d122fc44e7e28177127e2ce8881d54ba4718a064
SwabbieManolo <nassuunaswabrah@gmail.com>
2023-04-01 20:44:47 +0300

    Assignments, grid profile

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 //declare a variable called x
-/**
- * declare a variable and assign Swabbie
+/*
+  declare a variable and assign Swabbie
  */
 var x= "Swabbie"
 console.log(x)
+
+//For loop
+for(let i=1; i<=5; i++) {
+    console.log("code " + i)
+}
+//While loop
+i = 1
+while( i < 5) {
+    console.log("Code " + i)
+    i++
+}
+//Break & Continue
+i = 1
+while( i < 5) {
+    if(i == 2) continue
+    console.log("Code " + i)
+    if(i == 3) break
+    i++
+}
+    
 
 document.getElementById("Message").innerHTML = "Hooray, You have used Javascript!";
